b0c86388880b946be0d7b083cc9f6b04a2bfc5a9
bpdeveloperscommunity <bepracticaldeveloperscommunity@gmail.com>
2024-04-19 20:18:30 +0530

    all flew

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -6,10 +6,24 @@ function Signup() {
   return (
     <div className='container signup login p-3 p-md-5'>
         <div className="card shadow">
-            <h1 className="fs-3">Create New Account</h1>
+            <h1 className="fs-3">New Member Account </h1>
             <div className="form-group mt-3">
                 <label htmlFor="">username</label>
                 <input type="text" placeholder='Username' className="p-3 form-control" />
+            </div>
+            <div className="row">
+              <div className="col-6">
+              <div className="form-group mt-3">
+                <label htmlFor="">Designation </label>
+                <input type="text" placeholder='Type Your Designation' className="p-3 form-control" />
+            </div>
+              </div>
+              <div className="col-6">
+              <div className="form-group mt-3">
+                <label htmlFor="">Unit</label>
+                <input type="text" placeholder='Unit' className="p-3 form-control" />
+            </div>
+              </div>
             </div>
             <div className="form-group mt-3">
                 <label htmlFor="">Email address</label>
@@ -24,9 +38,55 @@ function Signup() {
                 <input type="password" placeholder='create password' className="p-3 form-control" />
             </div>
             <div className="form-group mt-3">
-                <label htmlFor="">confrom password</label>
+                <label htmlFor="">confirm password</label>
                 <input type="password" placeholder='confirm password' className="p-3 form-control" />
             </div>
+            <div className="form-group mt-3">
+                <label htmlFor="">KGID NO</label>
+                <input type="password" placeholder='Enter KGID NO' className="p-3 form-control" />
+            </div>
+           <div className="mt-3">
+            <label htmlFor="" className='form-label'>Gender</label>
+           <div className="d-flex">
+         <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name='gender' id="inlineCheckbox1" value="option1"/>
+            <label class="form-check-label" for="inlineCheckbox1">Male</label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio"  name='gender' id="inlineCheckbox2" value="option2"/>
+            <label class="form-check-label" for="inlineCheckbox2">Female</label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name='gender' id="inlineCheckbox2" value="option2"/>
+            <label class="form-check-label" for="inlineCheckbox2">Other</label>
+          </div>
+         </div>
+           </div>
+         <div className="row">
+          <div className="col-6">
+          <div className="form-group mt-3">
+                <label htmlFor="">Date Of Birth</label>
+                <input type="date"  className="p-3 form-control" />
+            </div>
+          </div>
+          <div className="col-6">
+          <div className="form-group mt-3">
+                <label htmlFor="">Blood Group</label>
+                <select name="" id="" className="form-control p-3">
+                <option value="A" selected>A</option>
+                <option value="A+">A+</option>
+                <option value="A-">A-</option>
+                <option value="B+">B+</option>
+                <option value="B-">B-</option>
+                <option value="O+">O+</option>
+                <option value="O-">O-</option>
+              </select>
+
+            </div>
+          </div>
+         </div>
+           <div className="row">
+            <div className="col-6">
             <div className="form-group mt-3">
                 <label htmlFor="">Region/State</label>
               <select name="" id="" className="form-control p-3">
@@ -38,6 +98,19 @@ function Signup() {
                 <option value="">Maharastra</option>
               </select>
             </div>
+            </div>
+            <div className="col-6">
+            <div className="form-group mt-3">
+                <label htmlFor="">Gender</label>
+              <select name="" id="" className="form-control p-3">
+                <option value="">Male</option>
+                <option value="">Female</option>
+                <option value="">Other</option>
+              </select>
+            </div>
+            </div>
+       
+           </div>
             <div class="form-check mt-3">
             <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
             <label class="form-check-label" for="flexCheckDefault">
