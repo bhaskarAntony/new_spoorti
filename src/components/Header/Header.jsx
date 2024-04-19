5a89116de740ae74075dd5b1acc20f9eab4eb942
bpdeveloperscommunity <bepracticaldeveloperscommunity@gmail.com>
2024-04-19 20:29:42 +0530

    all flew

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -17,45 +17,49 @@ function Header() {
     
   return (
    <header>
-    <Navbar expand="lg" className="navbar">
-      <Container fluid>
-        <Navbar.Brand href="/" ><div className='d-flex gap-2 align-items-center logo'><img src={logo} alt="logo" /> <h1 className="fs-3 fw-bold text-white">SPORTI</h1></div></Navbar.Brand>
-      
-        <button className='btn p-0 bg-transparent text-white  d-block d-md-none' onClick={handleShow}><i className='bi bi-list fs-1'></i></button>
-        <Navbar.Collapse id="navbarScroll">
-         <div className="d-none d-md-block w-100">
-         <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
-    <Nav.Link href="/services">Home</Nav.Link>
-    <Nav.Link href="/about">About us</Nav.Link>
-    <NavDropdown title="Services" id="basic-nav-dropdown">
-        <NavDropdown.Item href="/food">Dining</NavDropdown.Item>
-        <NavDropdown.Item href="/room">Accomodation</NavDropdown.Item>
-        {/* <NavDropdown.Item href="/events">Events</NavDropdown.Item> */}
-        <NavDropdown.Item href="/view/1">Conference Hall</NavDropdown.Item>
-        <NavDropdown.Item href="/view/2">Main Event Hall</NavDropdown.Item>
-        <NavDropdown.Item href="/view/3">Mini Theatre</NavDropdown.Item>
-        <NavDropdown.Item href="/view/6">Barbeque</NavDropdown.Item>
-        <NavDropdown.Item href="/view/7">GYM</NavDropdown.Item>
-        <NavDropdown.Item href="/view/8">Badminton</NavDropdown.Item>
-        <NavDropdown.Item href="/view/9">Table Tennis</NavDropdown.Item>
-        <NavDropdown.Item href="/view/10">Billiards</NavDropdown.Item>
-        <NavDropdown.Item href="/view/11">Parking</NavDropdown.Item>
-        <NavDropdown.Item href="/services" className='text-primary'>View All Services</NavDropdown.Item>
-    </NavDropdown>
-    <Nav.Link href="/gallery/2">Events</Nav.Link>
-    <Nav.Link href="/faqs">Faqs</Nav.Link>
-    <Nav.Link href="/events&gallery">Gallery</Nav.Link>
-    <Nav.Link href="/contact">Contact us</Nav.Link>
-</Nav>
-         </div>
-        <div className="d-flex gap-2  d-none d-lg-block">
-            <a href='/login' className="main-btn  px-4">Login</a>
-            {/* <a href='/signup' className="btn-outline  px-4">New Member signup</a> */}
-            {/* <button className="btn btn-danger">Create account</button> */}
-        </div>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+  <Navbar expand="lg" className="navbar">
+  <Container fluid>
+    <Navbar.Brand href="/"><div className='d-flex gap-2 align-items-center logo'><img src={logo} alt="logo" /> <h1 className="fs-3 fw-bold text-white">SPORTI</h1></div></Navbar.Brand>
+
+    <button className='btn p-0 bg-transparent text-white d-block d-md-none' onClick={handleShow}><i className='bi bi-list fs-1'></i></button>
+    <Navbar.Collapse id="navbarScroll">
+      <div className="d-none d-md-block w-100">
+        <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
+          <Nav.Link href="/services">Home</Nav.Link>
+          <Nav.Link href="/about">About us</Nav.Link>
+          <NavDropdown title="Services" id="basic-nav-dropdown">
+            <NavDropdown.Item href="/food">Dining</NavDropdown.Item>
+            <NavDropdown.Item href="/room">Accomodation</NavDropdown.Item>
+            {/* <NavDropdown.Item href="/events">Events</NavDropdown.Item> */}
+            <NavDropdown.Item href="/view/1">Conference Hall</NavDropdown.Item>
+            <NavDropdown.Item href="/view/2">Main Event Hall</NavDropdown.Item>
+            <NavDropdown.Item href="/view/3">Mini Theatre</NavDropdown.Item>
+            <NavDropdown.Item href="/view/6">Barbeque</NavDropdown.Item>
+            <NavDropdown.Item href="/view/7">GYM</NavDropdown.Item>
+            <NavDropdown.Item href="/view/8">Badminton</NavDropdown.Item>
+            <NavDropdown.Item href="/view/9">Table Tennis</NavDropdown.Item>
+            <NavDropdown.Item href="/view/10">Billiards</NavDropdown.Item>
+            <NavDropdown.Item href="/view/11">Parking</NavDropdown.Item>
+            <NavDropdown.Item href="/services" className='text-primary'>View All Services</NavDropdown.Item>
+          </NavDropdown>
+          <Nav.Link href="/gallery/2">Events</Nav.Link>
+          <Nav.Link href="/faqs">Faqs</Nav.Link>
+          <Nav.Link href="/events&gallery">Gallery</Nav.Link>
+          <Nav.Link href="/contact">Contact us</Nav.Link>
+          <a href='/login' className="main-btn px-4 mx-3">Login</a>
+
+          <a href='/signup' className="btn-outline px-4 mx-3 text-white">New Member signup</a>
+
+
+        </Nav>
+      </div>
+      <div className="d-flex gap-2 d-none d-lg-block align-items-center flex-nowrap">
+        {/* <button className="btn btn-danger">Create account</button> */}
+      </div>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+
 
 
 
@@ -93,8 +97,8 @@ function Header() {
             <li><a href="/faqs"><i class="bi bi-stars"></i> Faqs</a></li>
             <li><a href="/events&gallery"><i class="bi bi-images"></i> Gallery</a></li>
             <li><a href="/contact"><i class="bi bi-person-lines-fill"></i> Contact us</a></li>
-            <li><a href="/login" className='main-btn w-100 p-3'>Login</a></li>
-            <li><a href="/signup" className='main-btn w-100 p-3 mt-3'>New Member Signup</a></li>
+            <li><a href="/login" className='btn btn-light w-100 p-3'>Login</a></li>
+            <li><a href="/signup" className='btn btn-light w-100 p-3 mt-3'>New Member Signup</a></li>
          </ul>
         </Offcanvas.Body>
       </Offcanvas>
