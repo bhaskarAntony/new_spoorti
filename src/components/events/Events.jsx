15213046610257b21444638efa2769c60fa88323
bpdeveloperscommunity <bepracticaldeveloperscommunity@gmail.com>
2024-05-04 00:35:23 +0530

    updated files

--- src/components/events/Events.jsx
+++ src/components/events/Events.jsx
@@ -6,17 +6,17 @@ import './style.css'
 function Events() {
   return (
     <div className=''>
-         <div className="contact-banner about-banner">
+         <div className="contact-banner">
             {/* <h1 className='fs-1 fw-bold text-center'>About us</h1>
             <p className="fs-6 text-center">
             SPORTI consists of a team of Senior Officers from various units of the Police Department. It also has a Working Committee who conduct various Conferences and Workshops to discuss the operations and functioning of the Institute. The team hosts periodic meetings every last Friday of the month for effective improvements of the Institute.
             </p> */}
              <div className="skew-container">
         <div className="skew-left">
-            <h1 className="fs-2 fw-bold">Events & Gallery</h1>
+            <h1 className="fs-2">Events & Gallery</h1>
         </div>
         <div className="skew-right d-flex align-items-center">
-        <h1 className="fs-2 fw-bold text-black">SPORTI</h1>
+        <h1 className="fs-2">SPORTI</h1>
         </div>
     </div>
     </div>
@@ -26,7 +26,7 @@ function Events() {
                    {
                     gallerydata.map((eventItem, eventIndex)=>(
                         <div className="col-12 col-md-4 mb-3" key={eventIndex}>
-                        <div className="event-card rounded-2 h-100 overflow-hidden bg-white">
+                        <div className="event-card text-center rounded-2 h-100 overflow-hidden bg-white">
                             <div className="event-card-top">
                                 <img src={eventItem.image} alt={eventItem.title} className='w-100 mb-2'/>
                             </div>
